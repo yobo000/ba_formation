eac6eeafa1773014e33e5c22be66e52ce48fe98e
yobo000 <yfrancesc@gmail.com>
2019-02-10 15:40:28 +0800

    add formation in growth

--- src/task/model1.py
+++ src/task/model1.py
@@ -14,6 +14,7 @@ import time
 
 # edge pramas 3
 M = 3  # 3
+PRECISION = 0.00001  # 由于random_sample只在(0,1)
 
 
 class DissNetowrk(object):
@@ -71,7 +72,7 @@ class DissNetowrk(object):
         deg, cnt = zip(*degreeCount.items())
         deg = np.array(deg)
         cnt = np.array(cnt)
-        connectivity = np.divide(cnt, N)
+        connectivity = np.divide(cnt, self.size_num)
         deg_log = deg  # np.log10(deg)
         deg_cnt = connectivity  # np.log10(connectivity)
         order = np.argsort(deg_log)
@@ -84,7 +85,7 @@ class DissNetowrk(object):
         # hist_data = np.histogram(opinions.values())
         bins = [0.01 * n for n in range(100)]
         ax2.hist(opinions, bins=bins)
-        self.filename += '-' + time.strftime("%d%m")
+        self.filename += '-' + time.strftime("%d%m") + ".png"
         plt.savefig(self.filename)
 
     @py_random_state(1)
@@ -125,6 +126,44 @@ class DissNetowrk(object):
             source += 1
         return self.graph
 
+    @py_random_state(1)
+    def barabasi_albert_with_opinion_graph_formation(self, seed=None):
+        """Returns a random graph according to the Barabási–Albert preferential
+        attachment model.
+        """
+        if self.growth < 1 or self.growth >= self.size_num:
+            raise nx.NetworkXError("Barabási–Albert network must have m >= 1"
+                                   " and m < n, m = %d, n = %d" % (self.growth, self.size_num))
+        # Add m initial nodes (m0 in barabasi-speak)
+        self.graph = empty_graph(self.init_num)
+        s = np.random.random_sample(self.init_num)
+        # print("initial value:", dict(enumerate(s)))
+        nx.set_node_attributes(self.graph, dict(enumerate(s)), 'opinion')
+        repeated_nodes = list(self.graph.nodes(data=False))
+        # Start adding the other n-m nodes. The first node is m.
+        source = self.growth
+        while source < self.size_num:
+            opinion_value = np.random.random_sample()
+            # Filter nodes from the targets
+            nodes = list(self.graph.nodes(data=True))
+            # pa_nodes = itemgetter(*targets)(nodes)
+            pa_nodes = self.opinion_filter(opinion_value, nodes)
+            targets = self._random_subset(pa_nodes, repeated_nodes, self.growth, seed)
+            # Add node with opinion
+            self.graph.add_node(source, opinion=opinion_value)
+            # Add edges to m nodes from the source.
+            self.graph.add_edges_from(zip([source] * self.growth, targets))
+            # Add one node to the list for each new edge just created.
+            repeated_nodes.extend(targets)
+            # And the new node "source" has m edges to add to the list.
+            repeated_nodes.extend([source] * self.growth)
+            # Now choose m unique nodes from the existing nodes
+            # Pick uniformly from repeated_nodes (preferential attachment)
+            # targets = _random_subset(repeated_nodes, m, seed)
+            self.opinion_formation_in_growth()
+            source += 1
+        return self.graph
+
     def formation(self, node1, node2):
         """
         value = node1.value + node2.value
@@ -133,7 +172,7 @@ class DissNetowrk(object):
         value2 = self.graph.node[node2]['opinion']
         diff = abs(value1 - value2)
         if diff < self.threshold and diff > PRECISION:
-            value_1 = value1 - self.prama * (value1 - value2)
+            value_1 = value1 - self.param * (value1 - value2)
             value_2 = value2 - self.param * (value2 - value1)
             return value_1, value_2
         elif diff < PRECISION:
@@ -173,13 +212,45 @@ class DissNetowrk(object):
                 break
         return self.graph
 
+    def opinion_formation_in_growth(self):
+        """
+        1. 随机取edges
+        2. formation
+        3. 重复直到收敛
+        do 40 - 100 times for each node
+        """
+        loop = 1
+        # counter = 0
+        # not_convergence = True
+        loop_num = np.random.randint(40, 120)
+        while loop < loop_num:
+            # G.edges 返回一个有两个node编号的tuple
+            edges = list(self.graph.edges(data=False))
+            if edges:
+                edge = random.choice(edges)
+                # random edges
+                value1, value2 = self.formation(*edge)
+                if value2:
+                    node1, node2 = edge
+                    self.graph.node[node1]['opinion'] = value1
+                    self.graph.node[node2]['opinion'] = value2
+                elif value1:
+                    pass
+                else:
+                    # remove edge
+                    self.graph.remove_edge(*edge)
+                loop += 1
+            else:
+                break
+        return self.graph
+
     def upload_file(self, bucket_name, access_token):
         url = "https://www.googleapis.com/upload/storage/v1/b/" + bucket_name + "/o"
         params = {
             'uploadType': "media",
             'name': self.filename
         }
-        data = open('.' + self.filename, 'rb').read()
+        data = open('./' + self.filename, 'rb').read()
         headers = {
             'Authorization': 'Bearer {}'.format(access_token),
             "Content-Type": "image/png"
--- src/task/run.py
+++ src/task/run.py
@@ -1,8 +1,24 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, request
 from celery import Celery
-from tasks import function1
+# from tasks import function1
 import logging
+from utils import get_access_token, list_buckets
+from model1 import DissNetowrk
+
+
+N = 3000  # 10000
+# edge pramas 3
+M = 3  # 3
+# growth node number 50
+M_0 = 30
+# iter times 10
+T = 1
+# perferential attachment tolerant
+THERSHOLD = TOLERANT = 0.3
+# deffuant tolerant
+DEFFUANT_COEFF = 0.5
+
 
 logging.basicConfig(
     level=logging.INFO,
@@ -18,23 +34,75 @@ celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
 
 
-@app.route('/', methods=['POST'])
-def longtask():
-    func_id = request.args.get('func')
-    project_id = request.args.get('id')
-    size_num = request.args.get('size')
-    init_num = request.args.get('init')
-    loop_num = request.args.get('loop')
-    threshold = request.args.get('threshold')
-    param = request.args.get('prama')
-    if func_id == 1:
-        task = function1.apply_async(
-            project_id=project_id,
-            size_num=size_num,
-            init_num=init_num,
-            loop_num=loop_num,
-            threshold=threshold,
-            param=param)
-        return {"result": task.id}
-    else:
-        return {"result": "null"}
+@celery.task
+def function1(project_id="", size_num=N, init_num=M_0, loop_num=50000, threshold=THERSHOLD, param=DEFFUANT_COEFF):
+    # do once
+    network = DissNetowrk(
+        size_num=size_num,
+        init_num=init_num,
+        loop_num=loop_num,
+        threshold=threshold,
+        param=param)
+    network.barabasi_albert_with_opinion_graph()
+    network.opinion_formation()
+    network.save_distribution()
+    access_token = get_access_token()
+    buckets = list_buckets(project_id, access_token)
+    bucket_name = buckets["items"][0]["id"]
+    response = network.upload_file(bucket_name, access_token)
+    return response
+
+
+@celery.task
+def function2(project_id="", size_num=N, init_num=M_0, loop_num=50000, threshold=THERSHOLD, param=DEFFUANT_COEFF):
+    # for each node is adding
+    network = DissNetowrk(
+        size_num=size_num,
+        init_num=init_num,
+        loop_num=loop_num,
+        threshold=threshold,
+        param=param)
+    network.barabasi_albert_with_opinion_graph_formation()
+    network.save_distribution()
+    access_token = get_access_token()
+    buckets = list_buckets(project_id, access_token)
+    bucket_name = buckets["items"][0]["id"]
+    response = network.upload_file(bucket_name, access_token)
+    return response
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return 'flask'
+    elif request.method == 'POST':
+        func_id = request.args.get('func')
+        project_id = request.args.get('id')
+        size_num = int(request.args.get('size'))
+        init_num = int(request.args.get('init'))
+        loop_num = int(request.args.get('loop'))
+        threshold = float(request.args.get('threshold'))
+        param = float(request.args.get('param'))
+        if func_id == "1":
+            task = function1.apply_async(kwargs={
+                "project_id": project_id,
+                "size_num": size_num,
+                "init_num": init_num,
+                "loop_num": loop_num,
+                "threshold": threshold,
+                "param": param})
+            return task.task_id
+        elif func_id == "2":
+            task = function2.apply_async(kwargs={
+                "project_id": project_id,
+                "size_num": size_num,
+                "init_num": init_num,
+                "loop_num": loop_num,
+                "threshold": threshold,
+                "param": param})
+        else:
+            return "null"
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=True)
